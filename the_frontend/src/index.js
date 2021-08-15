63a1611f0c710acc5a4ca0fcc526d31acca45cc2
ajfame11 <ajfame11@gmail.com>
2021-08-15 14:29:48 -0400

    delete

--- the_frontend/src/index.js
+++ the_frontend/src/index.js
@@ -8,7 +8,7 @@ import { createStore, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
 import { composeWithDevTools } from 'redux-devtools-extension'
 import {BrowserRouter as Router, Link, Redirect, Route, Switch} from 'react-router-dom';
-import rootReducer from './reducers/rootReducer'
+import rootReducer from './reducers'
 
 const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)))
 
